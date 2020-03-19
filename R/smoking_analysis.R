7a2b05e1fb25b1832027f25ada2803814feca087
E-R <elior.rahmani@gmail.com>
2020-03-19 11:40:21 -0700

    updated the smoking analysis to include full EWAS to test methods for calibration

--- R/smoking_analysis.R
+++ R/smoking_analysis.R
@@ -36,21 +36,18 @@ prep_smoking_data <- function(data_path, hannum_smk_status_path){
     ref <- as.matrix(EpiDISH::centDHSbloodDMC.m[,c("Neutro","Eosino","CD4T","CD8T","Mono","B","NK")])
     W.liu <- EpiDISH::epidish(X.liu, ref)$estF
     
-    #save(X.liu, smk.liu, cov.liu, W.liu, ref.liu, file = file_name1)
     liu <- list(X=X.liu,
                 smk=smk.liu,
                 cov=cov.liu,
                 W=W.liu)
     save(liu, file=file_name1)
-    rm(gse, X.liu, smk.liu, cov.liu, ref.liu)
+    rm(gse, X.liu, smk.liu, cov.liu, W.liu, liu)
   }
   
   file_name2 <- paste(data_path,"hannum.RData",sep="/")
   if (!file.exists(file_name2)){
     # Download the Hannum et al. data 
     gse <- GEOquery::getGEO("GSE40279", destdir = data_path, GSEMatrix = TRUE)
-    
-    ## TODO download from github instead
     # smoking status; consider never-smokers [0], ex-smokers [1] and current smokers [2] (refer to occasional smokers as smokers)
     smk.hannum <- read.table(hannum_smk_status_path, header = T, row.names = 1, sep=",")
     # remove samples with NA values for smoking
@@ -81,8 +78,7 @@ prep_smoking_data <- function(data_path, hannum_smk_status_path){
                    cov=cov.hannum,
                    W=W.hannum)
     save(hannum, file=file_name2)
-    #save(X.hannum, smk.hannum, cov.hannum, W.hannum, ref.hannum, file = file_name2)
-    rm(gse, X.hannum, smk.hannum, cov.hannum, ref.hannum)
+    rm(gse, X.hannum, smk.hannum, cov.hannum, W.hannum)
     
   }
   
@@ -93,46 +89,74 @@ prep_smoking_data <- function(data_path, hannum_smk_status_path){
 #' Smoking analysis of methylation data
 #'
 #' Performs cell-type-specific analysis of blood methylation data in the context of smoking.
-#' Provides p-values estimated by CellDMC and TCA while correcting for given covariates
-#' as well as ReFACTor components. Combines cell types into myeloid and lymphoid compartments
+#' Provides p-values estimated by CellDMC and TCA while correcting for known covariates
+#' as well unknown technical variation using PCs calculated from control probes. Combines cell types into myeloid and lymphoid compartments
 #' for analysis.
 #'
 #' @param X Methylation values in matrix with sites as rows and samples as columns
 #' @param smk Smoking status encoded as 0 (non-smoker), 1 (ex-smoker), or 2 (smoker) in
 #'            a matrix with rownames as samples and 1 column entitled \code{smoking}
-#' @param covs Covariates in matrix format. Rownames are samples and column names are
+#' @param C1 Covariates in matrix format; for covariates that may affect methylation at the cell-type level. Rownames are samples and column names are
+#'                   covariate names
+#' @param C2 Covariates in matrix format; for covariates that may affect the methylation mixtures. Rownames are samples and column names are
 #'                   covariate names
 #' @param W Cell fractions in a matrix with cell types as column names and samples as
 #'          row names. Expects cell types: "Neutro","Eosino","Mono","CD4T","CD8T","B","NK"
 #' @param smoking_cpgs List of CpG names to analyze for associations
 #' @param dataset.name Name of dataset for naming output variables.
 #' @return A list of results. Each slot contains p-values output by each method
-run_smoking_analysis <- function(X, smk, covs, W, smoking_cpgs, dataset.name){
+run_smoking_analysis <- function(X, smk, C1, C2, W, smoking_cpgs, dataset.name){
   # combine cell types into myeloid and lymphoid compartments
   W.2 <- cbind(rowSums(W[,c("Neutro","Eosino","Mono")]),
                rowSums(W[,c("CD4T","CD8T","B","NK")]))
   colnames(W.2) <- c("MYE","LYM")
   
-  # Compute the ReFACTor components of the data as additional covariates
-  refactor.components <- TCA::refactor(X, k=6, rand_svd=T, C=covs)
+  # calculate PCs from low variance probes , to be treated as control probes (as in Lenhe et al. 2015); here, since we don't work with IDAT files and therefore don't have actual control probes, we use sites with the least variation in the data as control probes (as in Rahmani et al. 2019).
+  site.variances <- matrixStats::rowVars(X)
+  names(site.variances) <- rownames(X)
+  p <- 1000
+  low.var.sites <- names(head(sort(site.variances), p))
+  low.var.pca <- prcomp(t(X[low.var.sites,]), center=TRUE, scale=TRUE, rank=10)
   
-  # run celldmc and tca and extract p-values
-  celldmc.res <- CellDMC(X[smoking_cpgs,], smk, W.2,
-                         cov.mod = cbind(covs,refactor.components$scores))
+  C2_ <- if (is.null(C2)) low.var.pca$x else cbind(C2, low.var.pca$x)
+  
+  # remove polymorphic or cross-reactive sites and non-autosomal sites
+  nonspecific_probes <- read.table("https://raw.githubusercontent.com/cozygene/glint/master/parsers/assets/nonspecific_probes.txt")[,1]
+  XY_probes <- read.table("https://raw.githubusercontent.com/cozygene/glint/master/parsers/assets/HumanMethylationSites_X_Y.txt")[,1]
+  polymorphic_probes <- read.table("https://raw.githubusercontent.com/cozygene/glint/master/parsers/assets/polymorphic_cpgs.txt")[,1]
+  
+  # remove sites with very low variance that are unlikely to exhibit biological signal
+  low_var_sites <- names(which(site.variances<0.0001))
+  
+  exclude <- union(nonspecific_probes,union(XY_probes,union(low_var_sites,polymorphic_probes)))
+
+  X.final <- X[setdiff(rownames(X),exclude),]
+  message(sprintf("%i sites in final data; genome-wide significance at -log10 scale: %f", nrow(X.final),-log10(0.05/nrow(X.final))))
+  
+  message("Run celldmc and tca and extract p-values for the 7 smoking cpgs...")
+  celldmc.res <- CellDMC(X.final[smoking_cpgs,], smk, W.2, cov.mod = cbind(C1, C2_))
   celldmc.pvals <- list(celldmc.res$coe$MYE$p, celldmc.res$coe$LYM$p)
+  tca.mdl <- tca(X = X.final[smoking_cpgs,], W = W.2, C1 = cbind(C1, smk), C2 = C2_)
+  tca.pvals <- list(tca.mdl$gammas_hat_pvals[,"MYE.smk"], tca.mdl$gammas_hat_pvals[,"LYM.smk"])
+  tca.pvals.joint <- list(tca.mdl$gammas_hat_pvals.joint[,"smk"])
   
-  # run tca and extract p-values
-  tca.mdl <- tca(X = X[smoking_cpgs,], W = W.2,
-                 C1 = cbind(covs, smk),
-                 C2 = refactor.components$scores)
-  tca.pvals <- list(tca.mdl$gammas_hat_pvals[,"MYE.smoking"], 
-                    tca.mdl$gammas_hat_pvals[,"LYM.smoking"])
-  tca.pvals.joint <- list(tca.mdl$gammas_hat_pvals.joint[,"smoking"])
+  message("Perform a full cell-type-specific EWAS in order to asses calibration of TCA and CellDMC...")
+  tca.mdl.full <- tca(X = X.final, W = W.2, C1 = cbind(C1, smk), C2 = C2_)
+  tca.pvals.full <- list(tca.mdl.full$gammas_hat_pvals[,"MYE.smk"], tca.mdl.full$gammas_hat_pvals[,"LYM.smk"])
+  tca.pvals.joint.full <- list(tca.mdl.full$gammas_hat_pvals.joint[,"smk"])
+  celldmc.res.full <- CellDMC(X.final, smk, W.2, cov.mod = cbind(C1, C2_))
+  celldmc.pvals.full <- list(celldmc.res.full$coe$MYE$p, celldmc.res.full$coe$LYM$p)
   
   res.list <- list()
+  
   res.list[[sprintf("celldmc.pvals.%s", dataset.name)]] <- celldmc.pvals
   res.list[[sprintf("tca.pvals.%s", dataset.name)]] <- tca.pvals
   res.list[[sprintf("tca.pvals.joint.%s", dataset.name)]] <- tca.pvals.joint
+  
+  res.list[[sprintf("celldmc.pvals.full.%s", dataset.name)]] <- celldmc.pvals.full
+  res.list[[sprintf("tca.pvals.full.%s", dataset.name)]] <- tca.pvals.full
+  res.list[[sprintf("tca.pvals.joint.full.%s", dataset.name)]] <- tca.pvals.joint.full
+  
   return(res.list)
 }
 
@@ -192,7 +216,7 @@ plot_smoking_heatmap <- function(cpgs, pvals.liu, pvals.hannum, cell_types, meth
 #'            package under the same name
 #' @param hannum List generated by get_hannum_data() or provided in CellTypeSpecificMethylationData
 #'               package under the same name
-#' @param results_dir Directory to save plot of results
+#' @param results_dir Directory to save results and plots
 #' @param plot_type Extension for saving plot graphics, such as pdf or png
 #' @return A list of results. Slot \code{results} contains a list of p-values generated by each method.
 #'         Slot \code{plot} contains plot of results.
@@ -207,18 +231,25 @@ smoking_analysis <- function(liu, hannum, results_dir, plot_type){
   
   # Liu et al. analysis
   liu.smoking.res <- run_smoking_analysis(X = liu$X,
-                                          smk = liu$smk, 
-                                          covs = liu$cov, 
+                                          smk = liu$smk[,1], 
+                                          C1 = liu$cov, 
+                                          C2 = NULL,
                                           W = liu$W, 
                                           smoking_cpgs = smoking_cpgs, 
                                           dataset.name = "liu")
+  save(liu.smoking.res, file=paste(results_dir,"liu.smoking.res.RData",sep="/") )
+  
   # Hannum et al. analysis
+  C2.hannum <- matrix(0,nrow(hannum$cov),8)
+  for (i in 1:8) C2.hannum[,i] <- as.numeric(hannum$cov[,"plate"] == i)
   hannum.smoking.res <- run_smoking_analysis(X = hannum$X,
-                                             smk = hannum$smk, 
-                                             covs = hannum$cov, 
+                                             smk = hannum$smk[,1], 
+                                             C1 = hannum$cov[,c("age","gender")], 
+                                             C2 = C2.hannum,
                                              W = hannum$W, 
                                              smoking_cpgs = smoking_cpgs, 
                                              dataset.name = "hannum")
+  save(hannum.smoking.res, file=paste(results_dir,"hannum.smoking.res.RData",sep="/") )
   
   smoking.res <- c(liu.smoking.res,
                    hannum.smoking.res,
